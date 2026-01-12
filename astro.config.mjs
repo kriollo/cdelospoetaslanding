689155a13217942e8744fe19864649d33b07f959
kr107701984 <kriollone@gmail.com>
2026-01-11 22:48:05 -0300

    añade menu, efectos con gsap y url opengraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wwww.cdelospoetas.cl',
+  site: 'https://www.cdelospoetas.cl',
   vite: {
     plugins: [tailwindcss()]
   },
