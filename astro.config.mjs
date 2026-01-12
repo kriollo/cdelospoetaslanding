79b9ca763d409e23dbb477dca4c36077c85508aa
kr107701984 <kriollone@gmail.com>
2026-01-11 22:54:36 -0300

    fix opengraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.cdelospoetas.cl',
+  site: 'https://www.cdlospoetas.cl/',
   vite: {
     plugins: [tailwindcss()]
   },
